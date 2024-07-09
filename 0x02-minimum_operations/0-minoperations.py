55672b751aa1e232d76a73a9ccb24e74a37fcf5d
Fahleh <eddyukpong@gmail.com>
2024-07-09 16:27:53 +0100

    Task complete

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module for Minimum Operations"""
+
+
+def minOperations(n):
+    """
+    Returns the fewest number of operations needed to
+    result in exactly n H characters.
+    """
+    # Output must be at least 2 chars (copy all & paste.)
+    if (n < 2):
+        return 0
+    num_of_ops, base_num = 0, 2
+    while base_num <= n:
+        # While base_num is a factor of n (divides n with no remainder)
+        while n % base_num == 0:
+            # Incement num_of_ops by base_num
+            num_of_ops += base_num
+            # set n to the remainder
+            n //= base_num
+            # reduce base_num to find remaining smaller vals that evenly-divide n
+            # base_num -= 1
+        # If base_num is not a factor, increment it until it becomes one.
+        base_num += 1
+    return num_of_ops
