01eca91eb970c80a3ca182a4a32237caa85621ae
Fahleh <eddyukpong@gmail.com>
2024-07-17 13:59:12 +0100

    Fixes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,36 +12,36 @@ def custom_print(dict_sc, total_file_size):
             print("{}: {}".format(key, val))
 
 
-size = 0
-code = 0
-count = 0
+SIZE = 0
+CODE = 0
+COUNT = 0
 status_dict = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
 
 try:
     for line in sys.stdin:
-        curr_line = line.split()  # ✄ trimming
-        curr_line = curr_line[::-1]  # inverting
+        curr_line = line.split()
+        stripped_line = curr_line[::-1]
 
-        if len(curr_line) > 2:
-            count += 1
+        if len(stripped_line) > 2:
+            COUNT += 1
 
-            if count <= 10:
-                size += int(curr_line[0])  # file size
-                code = curr_line[1]  # status code
+            if COUNT <= 10:
+                SIZE += int(stripped_line[0])  # file size
+                CODE = stripped_line[1]  # status code
 
-                if (code in status_dict.keys()):
-                    status_dict[code] += 1
+                if (CODE in status_dict):
+                    status_dict[CODE] += 1
 
-            if (count == 10):
-                custom_print(status_dict, size)
-                count = 0
+            if (COUNT == 10):
+                custom_print(status_dict, SIZE)
+                COUNT = 0
 
 finally:
-    custom_print(status_dict, size)
+    custom_print(status_dict, SIZE)
