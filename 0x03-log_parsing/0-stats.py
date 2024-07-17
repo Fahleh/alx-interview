37c8116cf216516663e59945c4ea7880f606de3a
Fahleh <eddyukpong@gmail.com>
2024-07-17 14:57:30 +0100

    Fixes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,10 +7,10 @@ import signal
 def custom_print(dict_sc, total_file_size):
     """Method for printing in a particular format."""
 
-    print("File size: {}".format(total_file_size))
+    print(f"File size: {total_file_size}")
     for key, val in sorted(dict_sc.items()):
         if val != 0:
-            print("{}: {}".format(key, val))
+            print(f"{key}: {val}")
 
 
 def handle_signal(sig, frame):
@@ -52,24 +52,6 @@ try:
 
         if COUNT % 10 == 0:
             custom_print(status_dict, SIZE)
-
-        # curr_line = line.split()
-        # stripped_line = curr_line[::-1]
-
-        # if len(stripped_line) > 2:
-        #     COUNT += 1
-
-        #     if COUNT <= 10:
-        #         SIZE += int(stripped_line[0])  # file size
-        #         CODE = stripped_line[1]  # status code
-
-        #         if (CODE in status_dict):
-        #             status_dict[CODE] += 1
-
-        #     if (COUNT == 10):
-        #         custom_print(status_dict, SIZE)
-        #         COUNT = 0
-        #         SIZE = 0
 
 finally:
     custom_print(status_dict, SIZE)
