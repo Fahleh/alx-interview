2680a3b74617d1a3dfcb979e18266c5f3ffd6d8d
Fahleh <eddyukpong@gmail.com>
2024-08-21 17:24:18 +0100

    Fixes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Module for make change function
 """
@@ -11,19 +12,14 @@ def makeChange(coins, total):
       : Fewest number of coins needed to meet total.
       : 0 If total is 0 or less.
       : -1 if otal cannot be met by any number of coins you have.
-
     """
     if total <= 0:
         return 0
-
     if not coins:
         return -1
-
     coins_needed = [float("inf")] * (total + 1)
     coins_needed[0] = 0
-
     for coin in coins:
         for i in range(coin, total + 1):
             coins_needed[i] = min(coins_needed[i], coins_needed[i - coin] + 1)
-
     return coins_needed[total] if coins_needed[total] != float("inf") else -1
