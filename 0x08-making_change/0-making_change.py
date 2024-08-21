ed711be0a564eb3bd7bf68938642ddf306c9fccf
Fahleh <eddyukpong@gmail.com>
2024-08-21 17:04:10 +0100

    Task completed

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    Returns: 
+      : Fewest number of coins needed to meet total.
+      : 0 If total is 0 or less.
+      : -1 if otal cannot be met by any number of coins you have.
+
+    """
+    if total <= 0:
+        return 0
+
+    if not coins:
+        return -1
+
+    coins_needed = [float('inf')] * (total + 1)
+    coins_needed[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            coins_needed[i] = min(coins_needed[i], coins_needed[i - coin] + 1)
+
+    return coins_needed[total] if coins_needed[total] != float('inf') else -1
