0c60c30ef90a9c1e567555a04207c3992dbdbf22
Fahleh <eddyukpong@gmail.com>
2024-08-21 17:13:18 +0100

    Fixes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,30 +1,22 @@
-def makeChange(coins, total):
+def makecount(coins, total):
     """
     Given a pile of coins of different values,
     Returns: 
       : Fewest number of coins needed to meet total.
       : 0 If total is 0 or less.
-      : -1 if otal cannot be met by any number of coins you have.
+      : -1 if total cannot be met by any number of coins you have.
 
     """
-    if total <= 0:
-        return 0
-
     if not coins:
         return -1
-
-    balance = total
+    if total <= 0:
+        return 0
     count = 0
-    index = 0
-    change = sorted(coins, reverse=True)
-    n = len(coins)
-
-    while balance > 0:
-        if index >= n:
-            return -1
-        if balance - change[index] >= 0:
-            balance -= change[index]
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
             count += 1
-        else:
-            index += 1
-    return count
+        if (total == 0):
+            return count
+    return -1
