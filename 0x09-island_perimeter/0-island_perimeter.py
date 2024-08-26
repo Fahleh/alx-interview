3e17cd0752593cbb4a65a473f2f1e2e7a9d845bf
Fahleh <eddyukpong@gmail.com>
2024-08-26 17:19:07 +0100

    Task completed

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for the Island Perimeter Problem
+"""
+
+
+def island_perimeter(grid):
+    if not grid or not grid[0]:
+        return 0
+
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+
+                perimeter += 4
+
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+
+    return perimeter
