925aab961344c6f717062038ca4835e1dbbf95dd
Fahleh <eddyukpong@gmail.com>
2024-08-29 13:46:10 +0100

    Fixes

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,22 +4,20 @@
 
 
 def island_perimeter(grid):
-    if not grid or not grid[0]:
-        return 0
-
     perimeter = 0
-    rows, cols = len(grid), len(grid[0])
-
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-
-                perimeter += 4
-
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2
-
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2
-
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
